c283645347ce3bc5a487488e4128729640f920c8
Christerle Nounoute <christerlesn@gmail.com>
2020-10-21 18:09:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,10 @@ class Triangle
 
   def kind
     if @side_a < 0 || @side_b < 0 || @side_c < 0
+      begin
         raise TriangleError
+      rescue TriangleError
+      end
       elsif @side_a == @side_b + @side_c || @side_b == @side_a + @side_c || @side_c == @side_b + @side_a
         raise TriangleError
       elsif @side_a + @side_b < @side_c || @side_a + @side_c < @side_b || @side_b + @side_c < @side_a
